f7868f19f140f0679add4b5ab618a236ce2bb30b
EuniceMan <Ntoweunice702@gmail.com>
2021-09-16 12:42:41 +0000

    simple calculator

---
+++ README.md
@@ -0,0 +1,3 @@
+import 'dart:iO';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
---
+++ android/app/src/main/kotlin/com/womanrising/womanrisingss/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.womanrising.womanrisingss
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
